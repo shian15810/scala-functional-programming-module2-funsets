04d34d7ef98d905ed02567cc49b7ebfb7caedf6c
Cheeshian Chuah <shian15810@gmail.com>
2024-12-22 03:01:54 +0000

    explore to understand the questions and answers

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -12,28 +12,28 @@ trait FunSets extends FunSetsInterface:
   def contains(s: FunSet, elem: Int): Boolean = s(elem)
 
   /** Returns the set of the one given element. */
-  def singletonSet(elem: Int): FunSet = e => e == elem
+  def singletonSet(elem: Int): FunSet = x => x == elem
 
   /**
    * Returns the union of the two given sets, the sets of all elements that are
    * in either `s` or `t`.
    */
-  def union(s: FunSet, t: FunSet): FunSet = elem => s(elem) || t(elem)
+  def union(s: FunSet, t: FunSet): FunSet = x => s(x) || t(x)
 
   /**
    * Returns the intersection of the two given sets, the set of all elements
    * that are both in `s` and `t`.
    */
-  def intersect(s: FunSet, t: FunSet): FunSet = elem => s(elem) && t(elem)
+  def intersect(s: FunSet, t: FunSet): FunSet = x => s(x) && t(x)
 
   /**
    * Returns the difference of the two given sets, the set of all elements of
    * `s` that are not in `t`.
    */
-  def diff(s: FunSet, t: FunSet): FunSet = elem => s(elem) && !t(elem)
+  def diff(s: FunSet, t: FunSet): FunSet = x => s(x) && !t(x)
 
   /** Returns the subset of `s` for which `p` holds. */
-  def filter(s: FunSet, p: Int => Boolean): FunSet = elem => s(elem) && p(elem)
+  def filter(s: FunSet, p: Int => Boolean): FunSet = x => s(x) && p(x)
 
   /** The bounds for `forall` and `exists` are +/- 1000. */
   val bound = 1000
@@ -50,11 +50,10 @@ trait FunSets extends FunSetsInterface:
    * Returns whether there exists a bounded integer within `s` that satisfies
    * `p`.
    */
-  def exists(s: FunSet, p: Int => Boolean): Boolean = !forall(s, elem => !p(elem))
+  def exists(s: FunSet, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
 
   /** Returns a set transformed by applying `f` to each element of `s`. */
-  def map(s: FunSet, f: Int => Int): FunSet =
-    elem => exists(s, x => f(x) == elem)
+  def map(s: FunSet, f: Int => Int): FunSet = x => exists(s, y => f(y) == x)
 
   /** Displays the contents of a set */
   def toString(s: FunSet): String =
--- src/main/scala/funsets/FunSets.worksheet.sc
+++ src/main/scala/funsets/FunSets.worksheet.sc
@@ -8,11 +8,37 @@ printSet(s1)
 printSet(s2)
 printSet(s3)
 
-val s12 = union(s1, s2)
-val s23 = union(s2, s3)
+val s12  = union(s1, s2)
+val s23  = union(s2, s3)
+val s123 = union(s12, s23)
 
 printSet(s12)
 printSet(s23)
+printSet(s123)
 
 printSet(intersect(s12, s23))
 printSet(diff(s23, s12))
+
+forall(s123, x => x < 0) // false
+forall(s123, x => x < 1) // false
+forall(s123, x => x < 2) // false
+forall(s123, x => x < 3) // false
+forall(s123, x => x < 4) // true
+forall(s123, x => x > 0) // true
+forall(s123, x => x > 1) // false
+forall(s123, x => x > 2) // false
+forall(s123, x => x > 3) // false
+forall(s123, x => x > 4) // false
+
+exists(s123, x => x < 0) // false
+exists(s123, x => x < 1) // false
+exists(s123, x => x < 2) // true
+exists(s123, x => x < 3) // true
+exists(s123, x => x < 4) // true
+exists(s123, x => x > 0) // true
+exists(s123, x => x > 1) // true
+exists(s123, x => x > 2) // true
+exists(s123, x => x > 3) // false
+exists(s123, x => x > 4) // false
+
+printSet(map(s123, x => x + 1))
